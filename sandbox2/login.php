6e11bc93d14e4172656121c123499174a733094b
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-04-26 16:44:51 -0500

    sandbox2 added (merge)

---
+++ sandbox2/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Log in</title>
+
+		<link rel="stylesheet" href="packages/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="css/global.css">
+		<link rel="stylesheet" type="text/css" href="css/login.css">
+	</head>
+
+	<body>
+		<?php include 'header.html'; ?>
+
+		<div id="branding">
+			<img src="graphics/Census-Logo.png">
+			<h1>Census</h1>
+		</div>
+		<div id="credentials">
+			<input type="text" id="netid" placeholder="Net ID">
+			<input type="password" id="netidpass" placeholder="Password">
+			<input type="submit" id="submit" value="Log in" onclick="window.location='/sandbox/courses.php';">
+		</div>
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<script src="js/script.js"></script>
+	</body>
+
+</html>
